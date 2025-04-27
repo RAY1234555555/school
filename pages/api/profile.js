1cb7bfe068d619d55440129e00768182ee2d0a9e
Stanton <131873829+RAY1234555555@users.noreply.github.com>
2025-04-27 20:11:33 +0800

    Delete pages directory

--- pages/api/profile.js
+++
@@ -1,9 +0,0 @@
-import cookie from 'cookie'
-
-export default function handler(req, res) {
-  const cookies = cookie.parse(req.headers.cookie || '')
-  res.status(200).json({
-    name: cookies.oauthUsername || '',
-    email: cookies.oauthEmail || '',
-  })
-}
